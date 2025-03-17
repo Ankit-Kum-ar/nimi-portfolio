7a76164fc89913ee173ca289e09c0a2e103804d4
Sahil Rawat <sahilr8587@gmail.com>
2025-03-17 16:58:31 +0530

    add Footer component and integrate it into the App

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-top"></div>
+        <div className="footer-bottom flex justify-between bg-[#12243F] text-[#C4DAF3] px-20">
+            <div className="footer-left">
+                <p>© 2021 Builders All rights reserved</p>
+            </div>
+            <div className="footer-right">
+                <p>Terms & Conditions . Privacy Policy</p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
