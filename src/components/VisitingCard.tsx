bf6f5bddced74e03e5f182d8bc16fe8cb4a3a3d4
Sahil Rawat <sahilr8587@gmail.com>
2025-03-20 17:56:54 +0530

    enhance VisitingCard and VisitingcardBanner components; add contact details and improve layout responsiveness

--- src/components/VisitingCard.tsx
+++ src/components/VisitingCard.tsx
@@ -1,9 +1,32 @@
+import { Mail, Phone } from "lucide-react";
+import Logo from "/images/logo.png";
 
 const VisitingCard = () => {
+    const phoneNumber = "+918851469617";
   return (
-    <section className="visitingcardbanner bg-[white] w-full h-full rounded-2xl bg-[url(/images/Capa.png)] ">
+    <section className="visitingcardbanner w-full h-full rounded-2xl bg-[url(/images/Capa.png)] bg-white bg-no-repeat bg-contain flex flex-col justify-between  p-4">
+        <img src={Logo} alt="NIMI" className=" h-10 w-20" loading="lazy"/>
+        <p className="text-sm font-bold text-[#284368] ">NIMI DEVELOPERS PVT. LTD.</p>
+        <div>
+         <a
+        href={`tel:${phoneNumber}`}
+        className="flex items-center  shadow-lg cursor-pointer  transition-transform transform hover:scale-110"
+      >
+        <Phone className="text-[#284368] h-4 font-bold" />
+        <p className="text-sm text-[#284368] font-bold">{phoneNumber}</p>
+        </a>
+        <a
+        href="mailto:nimidevelopers2009@gmail.com"
+        className="flex items-center  shadow-lg cursor-pointer transition-transform transform hover:scale-110"
+      >
+        <Mail className="text-[#284368] h-4 font-bold" />
+        <p className="text-sm text-[#284368] font-bold">nimidevelopers2009@gmail.com</p>
+        </a>
+        </div>
+        <div className="flex justify-end ">
+        <p className="w-[70%] text-xs text-right text-[#284368] font-semibold">Plot No10, Main Road, Qutub Vihar-1, Dwarka Sector -19, New Delhi -110071</p>
+        </div>
 
-      
     </section>
   )
 }
--- src/components/VisitingcardBanner.tsx
+++ src/components/VisitingcardBanner.tsx
@@ -3,25 +3,25 @@ import BuilderIcon from "/images/BuilderIcon.png"
 import VisitingCard from "./VisitingCard"
 const VisitingcardBanner = () => {
   return (
-    <section className="visitingcardbanner bg-[#1E5AA0] w-full px-10 py-10  ">
+    <section className="visitingcardbanner bg-[#1E5AA0] w-full md:px-10 py-10 px-2  ">
         <div className="max-w-7xl mx-auto px-4 relative">
-            <div className="flex justify-center items-center absolute -top-16 left-10 h-10
+            <div className="flex md:flex-col  justify-center items-center absolute -top-16 left-10 h-10
             ">
                 <img src={BuilderIcon} alt="NIMI" className="h-24" loading="lazy" decoding="async" />
             </div>
-            <div className="banner flex flex-row gap-7 pt-10">
+            <div className="banner flex md:flex-row flex-col gap-7 pt-10">
                 <div className="content flex flex-col items-start gap-6">
                     <h5 className="text-[#C4DAF3] text-lg font-bold">GET STARTED</h5>
-                    <p className="w-[60%] text-[#C4DAF3] text-justify text-xl">Whether you’re buying, selling, or investing, we are here to guide you every step of the way. Contact us today and let’s make your real estate goals a reality.
+                    <p className="md:w-[70%] w-[100%] text-[#C4DAF3] text-justify md:text-xl text-sm">Whether you’re buying, selling, or investing, we are here to guide you every step of the way. Contact us today and let’s make your real estate goals a reality.
                     </p>
-                    <button className="p-2 pl-4 bg-[#C4DAF3] text-[#1E5AA0] rounded-full text-center items-center flex gap-4 font-medium">
+                    <button className="md:p-2 p-1 md:pl-4 pl-2 bg-[#C4DAF3] text-[#1E5AA0] rounded-full text-center items-center flex gap-4 font-medium">
                         Contact Us
-                        <div className="aspect-square p-2 bg-[#1E5AA0] text-[#C4DAF3] rounded-full">
+                        <div className="aspect-square md:p-2 p-1 bg-[#1E5AA0] text-[#C4DAF3] rounded-full">
                             <ArrowUpRight />
                         </div>
                     </button>
                 </div>
-                <div className="card h-40 w-[50vw] rounded-3xl flex justify-center items-center">
+                <div className="card h-50 md:w-[60vw] w-[90vw] rounded-3xl flex justify-center items-center">
                     <VisitingCard />
                 </div>
             </div>
