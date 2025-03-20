bd642be184675d84bfbe59e29872faa04450a642
Ankit-0009 <ankitcode2511@gmail.com>
2025-03-20 14:16:31 +0530

    add Product page and ProductCard component; include background image and update App routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Home from "./Pages/Home/Home"
+import Product from "./Pages/Product/Product"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
       <Footer />
     </>
---
+++ src/Pages/Product/Product.tsx
@@ -0,0 +1,14 @@
+import bg from "/images/Product-bg.png";
+const Product = () => {
+  return (
+    <div className="min-h-screen bg-[#90BAE9]/80">
+      <img src={bg} alt="Product-bg" />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-[#284368] text-5xl font-bold text-left w-full px-10">Our Projects</h1>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Product
