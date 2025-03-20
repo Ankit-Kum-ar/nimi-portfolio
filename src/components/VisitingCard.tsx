2a9559dba74bd8f63a86873b18ea106b6c050da1
Sahil Rawat <sahilr8587@gmail.com>
2025-03-20 18:00:08 +0530

    refactor VisitingCard component; remove shadow effect from contact links for improved aesthetics

--- src/components/VisitingCard.tsx
+++ src/components/VisitingCard.tsx
@@ -10,14 +10,14 @@ const VisitingCard = () => {
         <div>
          <a
         href={`tel:${phoneNumber}`}
-        className="flex items-center  shadow-lg cursor-pointer  transition-transform transform hover:scale-110"
+        className="flex items-center  cursor-pointer  transition-transform transform hover:scale-110"
       >
         <Phone className="text-[#284368] h-4 font-bold" />
         <p className="text-sm text-[#284368] font-bold">{phoneNumber}</p>
         </a>
         <a
         href="mailto:nimidevelopers2009@gmail.com"
-        className="flex items-center  shadow-lg cursor-pointer transition-transform transform hover:scale-110"
+        className="flex items-center cursor-pointer transition-transform transform hover:scale-110"
       >
         <Mail className="text-[#284368] h-4 font-bold" />
         <p className="text-sm text-[#284368] font-bold">nimidevelopers2009@gmail.com</p>
