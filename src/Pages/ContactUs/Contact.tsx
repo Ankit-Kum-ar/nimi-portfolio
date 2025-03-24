d5abdf886751bc0654a4d4ac15b5c74626ae1475
Sahil Rawat <sahilr8587@gmail.com>
2025-03-24 15:22:58 +0530

    updated contact us for better user experience

--- src/Pages/ContactUs/Contact.tsx
+++ src/Pages/ContactUs/Contact.tsx
@@ -1,14 +1,16 @@
 import VisitingcardBanner from "@/components/VisitingcardBanner";
-import bg from "/images/Product-bg.png";
-import gradient from "/images/product-gradient.png";
+// import bg from "/images/Product-bg.png";
+// import gradient from "/images/product-gradient.png";
+import GetInTouch from "./components/GetInTouch";
 
 const Contact = () => {
   return (
-    <div className="min-h-screen bg-[#90BAE9]/80">
-      <img src={bg} alt="Product-bg" className="relative z-20" />
-      <div className="absolute top-0 left-0 w-full z-0 opacity-70 md:block hidden">
+    <div className="min-h-screen bg-[#90BAE9]/80 ">
+      {/* <img src={bg} alt="Product-bg" className="relative z-20" /> */}
+      {/* <div className="absolute top-0 left-0 w-full z-0 opacity-70 md:block hidden">
           <img src={gradient} alt="" />
-      </div>
+      </div> */}
+      <GetInTouch />
       
       <VisitingcardBanner />
     </div>
