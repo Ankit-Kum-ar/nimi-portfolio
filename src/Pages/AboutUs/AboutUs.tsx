f28d701e9ee87fc03eff1b3bd1957e7a73ed6ff6
Sahil Rawat <sahilr8587@gmail.com>
2025-04-01 15:24:19 +0530

    add Heading component to About Us page; update styling for improved layout

--- src/Pages/AboutUs/AboutUs.tsx
+++ src/Pages/AboutUs/AboutUs.tsx
@@ -1,8 +1,10 @@
+import Heading from "@/components/Heading"
+
 const AboutUs = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 text-3xl font-bold text-gray-700">
-      About Us
-    </div>
+    <>
+      <Heading title="About Us" />
+    </>
   )
 }
 
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,7 +7,7 @@ interface HeadingProps {
 const Heading:React.FC<HeadingProps> = ({ title })=> {
   return (
     <div
-      className="w-full flex items-center justify-center text-white md:h-28 h-20 bg-custom-gradient-2 md:text-5xl text-3xl font-semibold font-poppins"
+      className="w-full flex items-center justify-start bg-[url('/images/backdrop.jpeg')] bg-repeat-x object-contain text-[#284368] md:h-28 h-20 md:text-5xl text-3xl font-semibold font-poppins max-w-7xl mx-auto px-4"
     >
       {title}
     </div>
