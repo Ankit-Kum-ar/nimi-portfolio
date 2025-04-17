ab0ef4f516841318cc9a2ba8c6baa2ae75680aae
Sahil Rawat <sahilr8587@gmail.com>
2025-04-17 11:09:04 +0530

    feat: update CityMap and Map components with new iframe sources for improved location display

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,7 +5,8 @@ const Map = () => {
     <div  className="flex flex-col md:flex-row gap-6 relative justify-center py-4 items-center bg-[#283D68] ">
     <div className="w-full rounded-lg overflow-hidden shadow-lg">
       <iframe
-         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d218.9842580341358!2d77.03551315385069!3d28.577325129222128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1aeb7d8d192d%3A0x8fd7a6d09c7e455c!2sNIMI%20GROUP!5e0!3m2!1sen!2sin!4v1742303871361!5m2!1sen!2sin"
+         src=
+         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d218.9842580341358!2d77.03551315385069!3d28.577325129222128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1aeb7d8d192d%3A0x8fd7a6d09c7e455c!2sNIMI%20GROUP!5e0!3m2!1sen!2sin!4v1742303871361!5m2!1sen!2sin"
         width="100%"
         height="450"
         style={{ border: 0 }}
